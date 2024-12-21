69de896e5795ad2094c526af9534917f65753856
AlexHol <alexhol.the.best@gmail.com>
2024-12-21 14:04:56 +0200

    leson 5

--- mein.py
+++ mein.py
@@ -1,14 +1,24 @@
+from fileinput import filename
+
 import telebot
 
 token = '7636094498:AAFlezEInLwscQvSYtBHzlWmVcwEKjyVoEo'
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(content_types=['text'])
-def test_text(message):
-    print(message)
+file_text = "bot_text.txt"
+file_number = "bot_number.txt"
 
-    msg = message.text + ' - this is a text message'
-    bot.send_message(message.chat.id, msg)
+@bot.message_handler(content_types=['text'])
+def is_text(message):
+    if message.text:
+        file_text = "bot_text.txt"
+    else:
+        file_number = "bot_number.txt"
+
+    with open(filename, 'a') as file:
+        file.write(message.text + '\n')
+
+    bot.send_message(message.chat.id, 'Збережено до файла')
 
 
 if __name__ == '__main__':
